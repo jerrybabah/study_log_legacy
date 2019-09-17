895c2215dd8cc779cdeeea872b22f208d1e0ec18
BABA HIRONOBU <ghdtjs0417@gmail.com>
2019-09-17 14:22:21 +0900

    Add: connect-history-api-fallback middlware

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var history = require('connect-history-api-fallback');
 
 var indexRouter = require('./routes/index');
 
@@ -11,8 +12,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
