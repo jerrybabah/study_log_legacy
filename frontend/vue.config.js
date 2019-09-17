5855aee63ce8538794bea1c62f46efdac22aa4d3
BABA HIRONOBU <ghdtjs0417@gmail.com>
2019-09-17 13:59:17 +0900

    Add: build directory, proxy

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   lintOnSave: false,
+  outputDir: '../backend/public',
+  assetsDir: 'static',
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
+  },
 };
