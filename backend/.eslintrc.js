d572e35dbb0c6ef2dac52d16169e0a59e5e62ed5
BABA HIRONOBU <ghdtjs0417@gmail.com>
2019-09-17 14:11:14 +0900

    Add: eslint airbnb version

---
+++ backend/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "root": true,
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "airbnb-base",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    }
+};
