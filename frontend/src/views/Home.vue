5c77850b4671f5c5f8cf73073d4f1bddcaba4774
BABA HIRONOBU <ghdtjs0417@gmail.com>
2019-10-04 14:47:52 +0900

    Add: vue-calendar-heatmap ui component

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CalendarHeatmap :values="values" :end-date="endDate" :tooltip-unit="tooltipUnit"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import { CalendarHeatmap } from 'vue-calendar-heatmap';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    CalendarHeatmap,
+  },
+  data() {
+    return {
+      values: [
+        {
+          date: '2019-10-01',
+          count: 3,
+          temp: 'hello',
+        },
+        {
+          date: '2019-09-24',
+          count: 2,
+        },
+      ],
+      endDate: '2019-10-04',
+      tooltipUnit: 'learnings',
+    };
   },
 };
 </script>
