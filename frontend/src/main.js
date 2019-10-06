6a18f2455635ede1dcf69e81c6eac8098df4e392
BABA HIRONOBU <ghdtjs0417@gmail.com>
2019-10-06 22:31:59 +0900

    Add: bootstap, fontawesome components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import './assets/css/bv-custom.css';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+library.add(faGithub);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
