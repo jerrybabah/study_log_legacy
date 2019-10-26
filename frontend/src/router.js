ec233ce6d7448ac53c02f25e454415695d2b2307
BABA HIRONOBU <ghdtjs0417@gmail.com>
2019-10-26 22:05:25 +0900

    Add: 데이터베이스의 데이터를 갱신하는 동안 기다릴 페이지

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import WaitingRoom from './views/WaitingRoom.vue';
 import NotFound from './views/NotFound.vue';
 
 Vue.use(Router);
@@ -13,6 +14,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/waiting_room',
+      name: 'waiting room',
+      component: WaitingRoom,
     },
     {
       path: '/404',
---
+++ frontend/src/views/WaitingRoom.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>데이터를 갱신하는 중입니다.</div>
+    <div v-if="success">성공!</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'waitingRoom',
+  data() {
+    return {
+      success: false,
+    };
+  },
+
+  created() {
+    const BASE_URL = process.env.VUE_APP_ROOT_API;
+
+    this.$http.put(`${BASE_URL}/studies/`)
+      .then(msg => console.log(msg))
+      .then(() => { this.success = true; })
+      .catch(err => console.log(err));
+  },
+};
+</script>
+
+<style>
+
+</style>
